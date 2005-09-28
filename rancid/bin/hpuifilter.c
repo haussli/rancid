c8241328d6989837a2829fd05c4b01cfbcf5a9ab
heas <heas@shrubbery.net>
2005-09-28 20:19:00 +0000

    - add -t option to specify the poll timeout, mostly just useful for debugging - handle SIGHUP separate from SIGCHLD, and take defaults for SIGTERM/SIGINT - allocate a pty for the child, mainly for ssh which will talk to the   controlling terminal (/dev/tty) for the password exchange.  The password   prompt would be read by expect, which then replies on stdout, that is read   by hpuifilter, and ssh wants the reply on /dev/tty.  This way, ssh doesnt   use /dev/tty. - add a version of openpty() for those systems without one. - add a version of unsetenv() for those without; unsetenv is used to unset   DISPLAY, so that ssh doesnt do something funny w/ an X app to get the   password.
    XXX there is some clean-up to do here; remove the trash I've left in trying
        to get the ttys flushed when expect or the client closes their end.

--- rancid/bin/hpuifilter.c
+++ rancid/bin/hpuifilter.c
@@ -21,51 +21,116 @@
  * point of hpfilter is to filter all the bloody vt100 (curses) escape codes
  * that the HP procurve switches belch out, which are a real bitch to handle
  * in hlogin.
+ *
+ * Modified openpty() from NetBSD:
+ * Copyright (c) 1990, 1993, 1994
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
 
-#define DFLT_TO	60				/* default timeout */
 
 #include "config.h"
 #include "version.h"
+
+#if HAVE_UNISTD_H
+# include <unistd.h>
+#endif
 
 #include <stdio.h>
 #include <limits.h>
 #include <fcntl.h>
 #include <poll.h>
+#if HAVE_PTY_H
+# include <pty.h>
+#endif
 #include <regex.h>
 #include <signal.h>
+#if HAVE_STRING_H
+# if !STDC_HEADERS && HAVE_MEMORY_H
+#  include <memory.h>
+# endif
+# include <string.h>
+#endif
+#if HAVE_STRINGS_H
+# include <strings.h>
+#endif
+#if HAVE_PTMX && HAVE_STROPTS_H
+# include <stropts.h>
+#endif
+#include <sys/ioctl.h>
+#include <sys/stat.h>
 #include <sys/time.h>
 #include <termios.h>
+#if HAVE_UTIL_H
+# include <util.h>
+#endif
 
 #define	BUFSZ	(LINE_MAX * 2)
 
-char		*progname;
-int		debug = 0;
+char		**environ,
+		*progname;
+int		child,
+		debug,
+		drain,
+		timeo = 5;				/* default timeout */
 
 int		filter __P((char *, int));
+RETSIGTYPE	reapchild __P((void));
+#if !HAVE_OPENPTY
+int		openpty(int *, int *, char *, struct termios *,
+			struct winsize *);
+#endif
+RETSIGTYPE	sighdlr __P((int));
+#if !HAVE_UNSETENV
+int		unsetenv __P((const char *));
+#endif
 void		usage __P((void));
 void		vers __P((void));
-RETSIGTYPE	reapchild __P((void));
 
 int
-main(int argc, char **argv)
+main(int argc, char **argv, char **ev)
 {
     extern char		*optarg;
     extern int		optind;
     char		ch,
 			hbuf[BUFSZ],		/* hlogin buffer */
-			*hbufp,
+			ptyname[FILENAME_MAX + 1],
 			tbuf[BUFSZ],		/* telnet/ssh buffer */
 			*tbufp;
     int			bytes,			/* bytes read/written */
-			child,
+			devnull,
 			rval = EX_OK,
-			r[2],			/* recv pipe */
-			s[2];			/* send pipe */
+			ptym,			/* master pty */
+			ptys;			/* slave pty */
     ssize_t		hlen = 0,		/* len of hbuf */
 			tlen = 0;		/* len of tbuf */
-    struct pollfd	pfds[4];
+    struct pollfd	pfds[3];
     struct termios	tios;
+
+    environ = ev;
 
     /* get just the basename() of our exec() name and strip a .* off the end */
     if ((progname = strrchr(argv[0], '/')) != NULL)
@@ -75,10 +140,15 @@ main(int argc, char **argv)
     if (strrchr(progname, '.') != NULL)
 	*(strrchr(progname, '.')) = '\0';
 
-    while ((ch = getopt(argc, argv, "dhv")) != -1 )
+    while ((ch = getopt(argc, argv, "dhvt:")) != -1 )
 	switch (ch) {
 	case 'd':
 	    debug++;
+	    break;
+	case 't':
+	    timeo = atoi(optarg);
+	    if (timeo < 1)
+		timeo = 1;
 	    break;
 	case 'v':
 	    vers();
@@ -94,22 +164,36 @@ main(int argc, char **argv)
 	return(EX_USAGE);
     }
 
-    /* reap our children */
-    signal(SIGCHLD, (void *) reapchild);
-    signal(SIGHUP, (void *) reapchild);
-    signal(SIGINT, (void *) reapchild);
-    signal(SIGTERM, (void *) reapchild);
+    unsetenv("DISPLAY");
 
-    /* create 2 pipes for send/recv and then fork and exec telnet/ssh */
+    /* allocate pty for telnet/ssh, then fork and exec */
     for (child = 3; child < 10; child++)
 	close(child);
-    if (pipe(s) || pipe(r)) {
-	fprintf(stderr, "%s: pipe() failed: %s\n", progname,
+    if (openpty(&ptym, &ptys, ptyname, NULL, NULL)) {
+	fprintf(stderr, "%s: could not allocate pty: %s\n", progname,
 		strerror(errno));
 	return(EX_TEMPFAIL);
     }
+    /* make the pty raw */
+    if (tcgetattr(ptys, &tios)) {
+	fprintf(stderr, "%s: tcgetattr() failed: %s\n", progname,
+		strerror(errno));
+	return(EX_OSERR);
+    }
+    tios.c_lflag &= ~ECHO;
+    tios.c_lflag &= ~ICANON;
+#ifdef VMIN
+    tios.c_cc[VMIN] = 1;
+    tios.c_cc[VTIME] = 0;
+#endif
+    if (tcsetattr(ptys, TCSANOW, &tios)) {
+	fprintf(stderr, "%s: tcsetattr() failed: %s\n", progname,
+		strerror(errno));
+	return(EX_OSERR);
+    }
 
-    /* if a tty, make it raw as the hp echos _everything_, including
+    /*
+     * if a tty, make it raw as the hp echos _everything_, including
      * passwords.
      */
     if (isatty(fileno(stdin))) {
@@ -135,162 +219,235 @@ main(int argc, char **argv)
     memset(hbuf, 0, BUFSZ);
     memset(tbuf, 0, BUFSZ);
 
+    /* reap our children, must be set-up *after* openpty() */
+    signal(SIGCHLD, (void *) reapchild);
+
     if ((child = fork()) == -1) {
-	fprintf(stderr, "%s: fork() failed: %s\n", progname,
-		strerror(errno));
+	fprintf(stderr, "%s: fork() failed: %s\n", progname, strerror(errno));
 	return(EX_TEMPFAIL);
     }
 
     if (child == 0) {
-	/* close the parent's side of the pipes; we write r[1], read s[0] */
-	close(s[1]);
-	close(r[0]);
-	/* close stdin/out/err and attach them to the pipes */
-	if (dup2(s[0], 0) == -1 || dup2(r[1], 1) == -1 || dup2(r[1], 2) == -1) {
-	    fprintf(stderr, "%s: dup2() failed: %s\n", progname,
-		    strerror(errno));
+	signal(SIGCHLD, SIG_DFL);
+	/* close the master pty & std* inherited from the parent */
+	close(ptym);
+	setsid();
+	if (ptys != 0)
+	    close(0);
+	if (ptys != 1)
+	    close(1);
+	if (ptys != 2)
+	    close(2);
+#ifdef TIOCSCTTY
+	if (ioctl(ptys, TIOCSCTTY, NULL) == -1) {
+	    snprintf(ptyname, FILENAME_MAX, "%s: could not set controlling "
+		     "tty: %s\n", progname, strerror(errno));
+	    write(ptys, ptyname, strlen(ptyname));
 	    return(EX_OSERR);
 	}
-	close(s[0]);
-	close(r[1]);
+#endif
+
+	/* close stdin/out/err and attach them to the pipes */
+	if (dup2(ptys, 0) == -1 || dup2(ptys, 1) == -1 || dup2(ptys, 2) == -1) {
+	    snprintf(ptyname, FILENAME_MAX, "%s: dup2() failed: %s\n", progname,
+		     strerror(errno));
+	    write(ptys, ptyname, strlen(ptyname));
+	    return(EX_OSERR);
+	}
+	if (ptys > 2)
+	    close(ptys);
 
 	/* exec telnet/ssh */
 	execvp(argv[optind], argv + optind);
-	fprintf(stderr, "%s: execvp() failed: %s\n", progname, strerror(errno));
+	snprintf(ptyname, FILENAME_MAX, "%s: execvp() failed: %s\n", progname,
+		 strerror(errno));
+	write(ptys, ptyname, strlen(ptyname));
 	return(EX_TEMPFAIL);
 	/*NOTREACHED*/
-    } else {
-	/* parent */
-	if (debug)
-	    fprintf(stderr, "child %d\n", child);
-
-	/* close the child's side of the pipes; we write s[1], read r[0] */
-	close(s[0]);
-	close(r[1]);
-
-	/* make FDs non-blocking */
-	if (fcntl(s[1], F_SETFL, O_NONBLOCK) ||
-	    fcntl(r[0], F_SETFL, O_NONBLOCK) ||
-	    fcntl(0, F_SETFL, O_NONBLOCK) ||
-	    fcntl(1, F_SETFL, O_NONBLOCK)) {
-	    fprintf(stderr, "%s: fcntl(NONBLOCK) failed: %s\n", progname,
-		strerror(errno));
-	    exit(EX_OSERR);
-	}
-
-	/* loop to read on stdin and r[0] */
-	hbufp = hbuf; tbufp = tbuf;
-
-#define	POLLEXP	(POLLERR | POLLHUP | POLLNVAL)
-	pfds[0].fd = fileno(stdin);
-	pfds[0].events = POLLIN | POLLEXP;
-	pfds[1].fd = fileno(stdout);
-	pfds[2].fd = r[0];
-	pfds[2].events = POLLIN | POLLEXP;
-	pfds[3].fd = s[1];
-
-	while (1) {
-	    /* if we have stuff in our buffer(s), we select on writes too */
-	    if (hlen)
-	        pfds[3].events = POLLOUT | POLLEXP;
-	    else
-	        pfds[3].events = POLLEXP;
-
-	    if (tlen)
-	        pfds[1].events = POLLOUT | POLLEXP;
-	    else
-	        pfds[1].events = POLLEXP;
-
-	    bytes = poll(pfds, 4, (DFLT_TO * 1000));
-	    if (bytes == 0)
-		/* timeout */
-		continue;
-	    if (bytes == -1) {
-		switch (errno) {
-		case EINTR:
-		    break;
-		default:
-		    rval = EX_IOERR;
-		    break;
-		}
-		continue;
-	    }
-
-	    /* write buffers first */
-	    /* write hbuf (stdin) -> s[1] */
-	    if ((pfds[3].revents & POLLOUT) && hlen) {
-		if ((bytes = write(s[1], hbuf, hlen)) < 0) {
-		    fprintf(stderr, "%s: write() failed: %s\n", progname,
-			    strerror(errno));
-		    close(pfds[3].fd);
-		} else if (bytes > 0) {
-		    strcpy(hbuf, hbuf + bytes);
-		    hlen -= bytes;
-		}
-	    } else if (pfds[3].revents & POLLEXP)
-		break;
-
-	    /* write tbuf -> stdout */
-	    if ((pfds[1].revents & POLLOUT) && tlen) {
-		/* if there is an escape char that didnt get filter()'d,
-		 * we need to write only up to that point and wait for
-		 * the bits that complete the escape sequence.  if at least
-		 * two bytes follow it, write it anyway as filter() didnt
-		 * match it.
-		 */
-		bytes = tlen;
-		if ((tbufp = index(tbuf, 0x1b)) != NULL)
-		    if (tlen - (tbufp - tbuf) < 2)
-			bytes = tbufp - tbuf;
-
-		if ((bytes = write(pfds[1].fd, tbuf, bytes)) < 0) {
-		    fprintf(stderr, "%s: write() failed: %s\n", progname,
-			    strerror(errno));
-		    close(pfds[1].fd);
-		} else if (bytes > 0) {
-		    strcpy(tbuf, tbuf + bytes);
-		    tlen -= bytes;
-		}
-	    } else if (pfds[1].revents & POLLEXP)
-		break;
-
-	    if (pfds[0].revents & POLLIN) {
-		/* read stdin into hbuf */
-		if (BUFSZ - hlen > 1) {
-		    bytes = read(pfds[0].fd, hbuf + hlen, (BUFSZ - 1) - hlen);
-		    if (bytes > 0) {
-			hlen += bytes;
-			hbuf[hlen] = '\0';
-		    } else if (bytes == 0 && errno != EAGAIN) {
-			/* EOF or read error */
-			pfds[0].events = 0;
-			close(pfds[0].fd);
-		    }
-		}
-	    }
-	    if (pfds[2].revents & POLLIN) {
-		/* read telnet/ssh into tbuf, then filter */
-		if (BUFSZ - tlen > 1) {
-		    bytes = read(pfds[2].fd, tbuf + tlen, (BUFSZ - 1) - tlen);
-		    if (bytes > 0) {
-			tbuf[tlen + bytes] = '\0';
-			tlen = filter(tbuf, tlen + bytes);
-		    } else if (bytes == 0 && errno != EAGAIN) {
-			/* EOF or read error */
-			pfds[2].events = 0;
-			close(pfds[2].fd);
-		    }
-		}
-	    }
-	}
-
-	close(0);
-	close(1);
-	close(s[1]);
-	close(r[0]);
     }
 
-    if (! kill(child, SIGINT))
+    /* parent */
+    if (debug)
+	fprintf(stderr, "child %d\n", child);
+
+    signal(SIGHUP, (void *) sighdlr);
+
+    /* close the slave pty */
+    close(ptys);
+
+    devnull = open("/dev/null", O_RDWR);
+
+    /* make FDs non-blocking */
+    if (fcntl(ptym, F_SETFL, O_NONBLOCK) ||
+	fcntl(fileno(stdin), F_SETFL, O_NONBLOCK) ||
+	fcntl(fileno(stdout), F_SETFL, O_NONBLOCK)) {
+	fprintf(stderr, "%s: fcntl(NONBLOCK) failed: %s\n", progname,
+		strerror(errno));
+	exit(EX_OSERR);
+    }
+
+    /* loop to read on stdin and ptym */
+#define	POLLEXP	(POLLERR | POLLHUP | POLLNVAL)
+    pfds[0].fd = fileno(stdin);
+    pfds[0].events = POLLIN | POLLEXP;
+    pfds[1].fd = fileno(stdout);
+    pfds[2].fd = ptym;
+    pfds[2].events = POLLIN | POLLEXP;
+
+    while (1) {
+	bytes = poll(pfds, 3, (timeo * 1000));
+	if (bytes == 0) {
+	    if (drain)
+		break;
+		/* timeout */
+	    continue;
+	}
+	if (bytes == -1) {
+	    switch (errno) {
+	    case EAGAIN:
+	    case EINTR:
+		break;
+	    default:
+		rval = EX_IOERR;
+		break;
+	    }
+	    continue;
+	}
+
+	/*
+	 * write buffers first
+	 * write hbuf (stdin) -> ptym
+	 */
+	if ((pfds[2].revents & POLLOUT) && hlen) {
+	    if ((bytes = write(pfds[2].fd, hbuf, hlen)) < 0 &&
+		errno != EINTR && errno != EAGAIN) {
+		fprintf(stderr, "%s: write() failed: %s\n", progname,
+			strerror(errno));
+		hbuf[0] = '\0';
+		hlen = 0;
+		drain = 1;
+		pfds[2].events &= ~POLLOUT;
+
+		break;
+	    } else if (bytes > 0) {
+		strcpy(hbuf, hbuf + bytes);
+		hlen -= bytes;
+		if (hlen < 1)
+		     pfds[2].events &= ~POLLOUT;
+	    }
+	} else if (pfds[2].revents & POLLEXP) {
+	    hbuf[0] = '\0';
+	    hlen = 0;
+	    pfds[2].events &= POLLIN;
+	    break;
+	}
+
+	/* write tbuf -> stdout */
+	if ((pfds[1].revents & POLLOUT) && tlen) {
+	    /*
+	     * if there is an escape char that didnt get filter()'d,
+	     * we need to write only up to that point and wait for
+	     * the bits that complete the escape sequence.  if at least
+	     * two bytes follow it, write it anyway as filter() didnt
+	     * match it.
+	     */
+	    bytes = tlen;
+	    if ((tbufp = index(tbuf, 0x1b)) != NULL)
+		if (tlen - (tbufp - tbuf) < 2)
+		    bytes = tbufp - tbuf;
+
+	    if ((bytes = write(pfds[1].fd, tbuf, bytes)) < 0 &&
+		errno != EINTR && errno != EAGAIN) {
+		fprintf(stderr, "%s: write() failed: %s\n", progname,
+			strerror(errno));
+		break;
+		tbuf[0] = '\0';
+		tlen = 0;
+		drain = 1;
+		pfds[1].events = 0;
+	    } else if (bytes > 0) {
+		strcpy(tbuf, tbuf + bytes);
+		tlen -= bytes;
+		if (tlen < 1)
+		    pfds[1].events &= ~POLLOUT;
+	    }
+	} else if (pfds[1].revents & POLLEXP) {
+	    break;
+	    tbuf[0] = '\0';
+	    tlen = 0;
+	    pfds[1].fd = devnull;
+	    pfds[1].events = 0;
+	}
+
+	/* read stdin -> hbuf */
+	if (pfds[0].revents & POLLIN) {
+	    if (BUFSZ - hlen > 1) {
+		bytes = read(pfds[0].fd, hbuf + hlen, (BUFSZ - 1) - hlen);
+		if (bytes > 0) {
+		    hlen += bytes;
+		    hbuf[hlen] = '\0';
+		    pfds[2].events |= POLLOUT;
+		} else if (bytes == 0 && errno != EAGAIN && errno != EINTR) {
+		    break;
+		    /* EOF or read error */
+		    drain = 1;
+		    pfds[0].fd = devnull;
+		    pfds[0].events = 0;
+		}
+	    }
+	} else if (pfds[0].revents & POLLEXP) {
+	    break;
+	    drain = 1;
+	    pfds[0].fd = devnull;
+	    pfds[0].events = 0;
+	}
+
+	/* read telnet/ssh -> tbuf, then filter */
+	if (pfds[2].revents & POLLIN) {
+	    if (BUFSZ - tlen > 1) {
+		bytes = read(pfds[2].fd, tbuf + tlen, (BUFSZ - 1) - tlen);
+		if (bytes > 0) {
+		    tlen += bytes;
+		    tbuf[tlen] = '\0';
+		    tlen = filter(tbuf, tlen);
+		    if (tlen > 0)
+			pfds[1].events |= POLLOUT;
+		} else if (bytes == 0 && errno != EAGAIN && errno != EINTR) {
+		    /* EOF or read error */
+		    break;
+		    drain = 1;
+		    pfds[2].fd = devnull;
+		    pfds[2].events = 0;
+		}
+	    }
+	} else if (pfds[2].revents & POLLEXP) {
+	    break;
+	    drain = 1;
+	    pfds[2].fd = devnull;
+	    pfds[2].events = 0;
+	}
+    }
+    /* try to flush buffers */
+    if (hlen) {
+	(void) write(pfds[2].fd, hbuf, hlen);
+	hlen = 0;
+    }
+    if (tlen) {
+	(void) write(pfds[1].fd, tbuf, tlen);
+	tlen = 0;
+    }
+    if ((bytes = read(pfds[2].fd, tbuf, (BUFSZ - 1))) > 0) {
+	tbuf[bytes] = '\0';
+	tlen = filter(tbuf, bytes);
+	(void) write(pfds[1].fd, tbuf, tlen);
+    }
+    tcdrain(pfds[1].fd);
+    if ((hlen = read(pfds[0].fd, hbuf, (BUFSZ - 1))) > 0) {
+	(void) write(pfds[2].fd, hbuf, hlen);
+    }
+    tcdrain(pfds[2].fd);
+
+    if (child && ! kill(child, SIGINT))
 	reapchild();
 
     return(rval);
@@ -390,19 +547,54 @@ reapchild(void)
     while ((pid = wait3(&status, WNOHANG, 0)) > 0)
 	if (debug)
             fprintf(stderr, "reap child %d\n", pid);
+    if (pid == child)
+	child = 0;
 
-    /*exit(1);*/
-return;
-
-    /* not reached */
+    return;
 }
+
+RETSIGTYPE
+sighdlr(int sig)
+{
+    if (debug)
+	fprintf(stderr, "GOT SIGNAL %d\n", sig);
+    drain = 1;
+    return;
+}
+
+#if !HAVE_UNSETENV
+int
+unsetenv(const char *name)
+{
+   char	**victim,
+	**end;
+   int	len;
+   if (environ == NULL)
+	return(0);
+   len = strlen(name);
+   victim = environ;
+   while (*victim != NULL) {
+	if (strncmp(name, *victim, len) == 0 && victim[0][len] == '=')
+	    break;
+	victim++;
+   }
+   if (*victim == NULL)
+	return(0);
+   end = victim + 1;
+   while (*end != NULL)
+	end++;
+   end--;
+   *victim = *end;
+   *end = NULL;
+   return(0);
+}
+#endif
 
 void
 usage(void)
 {
-    fprintf(stderr,
-"usage: %s [-hv] <telnet|ssh> [<ssh options>] <hostname> [<telnet_port>]\n",
-	progname);
+    fprintf(stderr, "usage: %s [-hv] [-t timeout] <telnet|ssh> [<ssh options>]"
+	    " <hostname> [<telnet_port>]\n", progname);
     return;
 }
 
@@ -412,3 +604,104 @@ vers(void)
     fprintf(stderr, "%s: %s version %s\n", progname, package, version);
     return;
 }
+
+
+#if !HAVE_OPENPTY
+#include <grp.h>
+#define TTY_LETTERS	"pqrstuvwxyzPQRST"
+#define TTY_OLD_SUFFIX	"0123456789abcdef"
+#define TTY_NEW_SUFFIX	"ghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+int
+openpty(int *amaster, int *aslave, char *name, struct termios *term,
+	struct winsize *winp)
+{
+    static char		line[] = "/dev/XtyXX";
+    const char		*cp1, *cp2, *cp, *linep;
+    int			master, slave;
+	gid_t ttygid;
+    mode_t		mode;
+    struct group	*gr;
+
+#if HAVE_PTMX
+    if ((master =
+#if HAVE_PTMX_BSD
+	 open("/dev/ptmx_bsd", O_RDWR))
+#else
+	 open("/dev/ptmx", O_RDWR))
+#endif
+				    != -1) {
+	linep = ptsname(master);
+	grantpt(master);
+	unlockpt(master);
+	if ((slave = open(linep, O_RDWR)) < 0) {
+	    slave = errno;
+	    (void) close(master);
+	    errno = slave;
+	    return(-1);
+	}
+#if HAVE_PTMX_OSF
+	{
+	    char buf[10240];
+	    if (ioctl (slave, I_LOOK, buf) != 0)
+		if (ioctl (slave, I_PUSH, "ldterm")) {
+		    close(slave);
+		    close(master);
+		    return(-1);
+		}
+	}
+#elif HAVE_STROPTS_H
+	ioctl(slave, I_PUSH, "ptem");
+	ioctl(slave, I_PUSH, "ldterm");
+	ioctl(slave, I_PUSH, "ttcompat");
+#endif
+	goto gotit;
+    }
+    if (errno != ENOENT)
+	return(-1);
+#endif
+
+    if ((gr = getgrnam("tty")) != NULL) {
+	ttygid = gr->gr_gid;
+	mode = S_IRUSR|S_IWUSR|S_IWGRP;
+    } else {
+	ttygid = getgid();
+	mode = S_IRUSR|S_IWUSR;
+    }
+
+    for (cp1 = TTY_LETTERS; *cp1; cp1++) {
+	line[8] = *cp1;
+	for (cp = cp2 = TTY_OLD_SUFFIX TTY_NEW_SUFFIX; *cp2; cp2++) {
+	    line[5] = 'p';
+	    line[9] = *cp2;
+	    if ((master = open(line, O_RDWR, 0)) == -1) {
+		if (errno != ENOENT)
+				continue;	/* busy */
+		if (cp2 - cp + 1 < sizeof(TTY_OLD_SUFFIX))
+				return -1; /* out of ptys */
+		else	
+				break;	/* out of ptys in this group */
+	    }
+	    line[5] = 't';
+	    linep = line;
+	    if (chown(line, getuid(), ttygid) == 0 &&
+		chmod(line, mode) == 0 &&
+		(slave = open(line, O_RDWR, 0)) != -1) {
+gotit:
+		*amaster = master;
+		*aslave = slave;
+		if (name)
+		    (void)strcpy(name, linep);
+		if (term)
+		    (void)tcsetattr(slave, TCSAFLUSH, term);
+		if (winp)
+		    (void)ioctl(slave, TIOCSWINSZ, winp);
+		return 0;
+	    }
+	    (void)close(master);
+	}
+    }
+    errno = ENOENT;	/* out of ptys */
+    return -1;
+}
+#endif
